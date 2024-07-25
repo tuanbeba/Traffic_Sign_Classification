cf0a3283f671ccccf0243db1524d26b0e90317cb
tuan1608 <tuan.1608.kc@gmail.com>
2024-07-25 17:58:24 +0700

    add model

--- model/trafficsignnet.py
+++ model/trafficsignnet.py
@@ -3,8 +3,9 @@ from tensorflow.keras import layers , models
 
 class TrafficSignNet:
     @staticmethod
-    def build(width, height, depth, classes):
-        inputs = tf.keras.Input(shape=(height, width, depth))
+    def build(width, height, channel, classes):
+        inputs = tf.keras.Input(shape=(height, width, channel))
+
         x = layers.Conv2D(8,(5,5), padding='same')(inputs)
         x= layers.BatchNormalization(axis= -1)(x)
         x = layers.Activation("relu")(x)
@@ -28,6 +29,23 @@ class TrafficSignNet:
         x = layers.Activation("relu")(x)
         x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 
-        model = models.Model(inputs=inputs, outputs=x)
+        # first set of FC => RELU layers
+        x = layers.Flatten()(x)
+        x = layers.Dense(128)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+        x = layers.Dropout(0.5)(x)
+
+        # second set of FC => RELU layers
+        x = layers.Dense(128)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+        x = layers.Dropout(0.5)(x)
+
+        # softmax classifier
+        outputs = layers.Dense(classes)(x)
+        outputs = layers.Activation("softmax")(outputs)
+
+        model = models.Model(inputs=inputs, outputs=outputs)
 
         return model
--- train.py
+++ train.py
@@ -1,1 +1,56 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+# import the necessary packages
+from model.trafficsignnet import TrafficSignNet
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report
+from skimage import transform
+from skimage import exposure
+from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import os
+
+def load_split(basePath, csvPath):
+	# initialize the list of data and labels
+	data = []
+	labels = []
+	# load the contents of the CSV file, remove the first line (since
+	# it contains the CSV header), and shuffle the rows (otherwise
+	# all examples of a particular class will be in sequential order)
+	rows = open(csvPath).read().strip().split("\n")[1:]
+	random.shuffle(rows)
+	# loop over the rows of the CSV file
+	for (i, row) in enumerate(rows):
+		# check to see if we should show a status update
+		if i > 0 and i % 1000 == 0:
+			print("[INFO] processed {} total images".format(i))
+		# split the row into components and then grab the class ID
+		# and image path
+		(label, imagePath) = row.strip().split(",")[-2:]
+		# derive the full path to the image file and load it
+		imagePath = os.path.join([basePath, imagePath])
+		image = io.imread(imagePath)
+		# resize the image to be 32x32 pixels, ignoring aspect ratio,
+		# and then perform Contrast Limited Adaptive Histogram
+		# Equalization (CLAHE)
+		image = transform.resize(image, (32, 32))
+		image = exposure.equalize_adapthist(image, clip_limit=0.1)
+		# update the list of data and labels, respectively
+		data.append(image)
+		labels.append(int(label))
+	
+	return (data, labels)
+
+ap = argparse.ArgumentParser()
+ap.add_argument('dataset', type=str, help='path to input dataset')
+ap.add_argument('name', type=str, help='Your name')
+ap.add_argument('name', type=str, help='Your name')
+
+
 
